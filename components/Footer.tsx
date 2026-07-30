5c23c06e07e1ee22c87334f1acc5784a39786476
hemanthvishnuakula <ahemanthvishnu@gmail.com>
2026-07-30 07:47:02 +0100

    Footer credit: title-case 'Domain and Email Security by Authex Labs'

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -62,7 +62,7 @@ export default function Footer() {
             <svg width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinejoin="round" aria-hidden="true">
               <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z" />
             </svg>
-            Domain &amp; email security by Authex Labs
+            Domain and Email Security by Authex Labs
           </a>
         </div>
       </div>
